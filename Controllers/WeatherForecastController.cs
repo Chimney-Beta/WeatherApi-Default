d9187dd300c7edd8775d6119557841cd0bd8d3bb
Jason Farrell <imjason@gmail.com>
2020-01-03 15:10:04 -0600

    Added Oily

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WeatherApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Humid"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Humid", "Oily"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
